ed9987f622c72512b203142394434228f76bfeeb
Rahee <raheetodkar.10@gmail.com>
2026-08-13 19:08:56 +0530

    Time: 0 ms (100%), Space: 16.5 MB (39.68%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,7 +10,60 @@
  */
 class Solution {
 public:
+    /* for queue:
+    void revUsingQueue(queue<int>& q,int k, queue<int>& ans, int unUsedElemCount){
+        //lets find whether we have k elem to reverse or not
+        if(unUsedElemCount >= k){
+            //1 case
+            stack<int> s;
+            for(int i=0;i<k;i++){
+                int elem = q.front();
+                q.pop();
+                s.push(elem);
+            }
+            //put them back in reversed order
+            while(!s.empty()){
+                ans.push(s.top());
+                s.pop();
+            }
+            //baki recursion
+            revUsingQueue(q,ans, k,unUsedElemCount-k);   //apn unUsedElemCount sobat 1 case handle keli, ata unUsedElemCount-k sobat recursion handle karel
+        }
+        else{
+            //no k grp to reverse, so return it as it is
+            for(int i=0;i<unUsedElemCount;i++){
+                int elem = q.front();
+                q.pop();
+                q.push(elem);
+            }
+            while(!q.empty()) {
+                ans.push(q.front());
+                q.pop();
+            }
+            return;
+        }
+    }*/
     ListNode* reverseKGroup(ListNode* head, int k) {
+        /* for queue:
+        queue<int>q;
+        queue<int> ans;
+        // Put linked-list elements into queue
+        ListNode* temp = head;
+        while(temp != NULL) {
+            q.push(temp->val);
+            temp = temp->next;
+        }
+        // Reverse groups
+        revUsingQueue(q, ans, k, q.size());
+        // Put queue values back into linked list
+        temp = head;
+        while(temp != NULL) {
+            temp->val = ans.front();
+            ans.pop();
+
+            temp = temp->next;
+        }
+        return head;*/
         //check group of len k
         int count=0;
         ListNode* temp = head;
@@ -44,6 +97,5 @@ public:
             //jr k size cha group exist nasel karat
             return head;
         }
-        
     }
 };
